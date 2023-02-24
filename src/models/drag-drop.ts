9a721d331e5f7d0a5c8c4f0f10e3139aa5518761
MarcosCInland <marcos.andres@inlandlogistics.co>
2023-02-24 16:24:38 -0600

    namespaces structures

---
+++ src/models/drag-drop.ts
@@ -0,0 +1,13 @@
+//  Namespaces & bundling
+namespace App{
+    export interface Draggable {
+        dragStartHandler(event: DragEvent): void;
+        dragEndHandler(event: DragEvent): void;
+    }
+    
+    export interface DragTarget {
+        dragOverHandler(event: DragEvent): void;
+        dropHandler(event: DragEvent): void;
+        dragLeaveHandler(event: DragEvent): void;
+    }
+}
